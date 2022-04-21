e998f53e37cbceee49919c2c02c3648912773e13
zeng_keqi <zeng_keqi@rivotek.com>
2022-04-21 14:35:49 +0800

    service

--- Service/src/main/java/com/example/service/BindActivity.java
+++ Service/src/main/java/com/example/service/BindActivity.java
@@ -2,13 +2,59 @@ package com.example.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class BindActivity extends AppCompatActivity {
+    private BindService service;
+    private boolean isBind = false;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.e("abc", "onServiceConnected: ");
+            service = ((BindService.LocalBinder) iBinder).getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.e("abc", "onServiceDisconnected: ");
+            service = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bind);
+
+        Button bind = findViewById(R.id.bind);
+        bind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isBind) {
+                    Intent intent = new Intent(BindActivity.this, BindService.class);
+                    bindService(intent,connection, Context.BIND_AUTO_CREATE);
+                    isBind = true;
+                }
+            }
+        });
+        Button unBind = findViewById(R.id.unBind);
+        unBind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBind) {
+                    isBind = false;
+                    unbindService(connection);
+                    service = null;
+                }
+            }
+        });
     }
 }
--- Service/src/main/java/com/example/service/BindService.java
+++ Service/src/main/java/com/example/service/BindService.java
@@ -1,30 +1,61 @@
 package com.example.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 public class BindService extends Service {
+    private NotificationManager mManager;
+    private int NOTIFICATION = R.string.local_service_started;
+    public final IBinder mBinder = new LocalBinder();
+
+    public class LocalBinder extends Binder{
+        public BindService getService(){
+            return BindService.this;
+        }
+    }
     @Override
     public void onCreate() {
         super.onCreate();
+        mManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        CharSequence sequence = getText(R.string.local_service_started);
+        PendingIntent intent = PendingIntent.getActivity(this,0,new Intent(this,BindService.class),0);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker(sequence)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle(getText(R.string.local_service_started))
+                .setContentText(sequence)
+                .setContentIntent(intent)
+                .build();
+        mManager.notify(NOTIFICATION,notification);
+        Log.e("abc", "onCreate: 通知栏已出");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.e("abc", "onDestroy: ");
+        mManager.cancel(NOTIFICATION);
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
+        Log.e("abc", "onUnbind: ");
         return super.onUnbind(intent);
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        Log.e("abc", "onBind: ");
         return null;
     }
 }
