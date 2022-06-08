3286137c57365290e40bd92a6757f69932c0ed66
zeng_keqi <zeng_keqi@rivotek.com>
2022-06-08 17:08:47 +0800

    [2022-06-08][MediaPlayer]Add playing media to clock[1/1]
    Signed-off-by: zeng_keqi <zeng_keqi@rivotek.com>

--- notify/src/main/java/com/example/notify/NotifyActivity.java
+++ notify/src/main/java/com/example/notify/NotifyActivity.java
@@ -8,9 +8,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.util.Log;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AlertDialog;
@@ -21,12 +21,16 @@ import java.util.Calendar;
 public class NotifyActivity extends AppCompatActivity {
     private static final String TAG = NotifyActivity.class.getSimpleName();
     private static Vibrator vibrator;
+    private String msg;
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notify);
         vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+//        uri = Uri.parse("/storage/emulated/0/qqmusic/song/北京室内男声合唱团 - 亮剑-中国军魂 [mqms2].mp3");
+//        uri = Uri.parse("https://www.kugou.com/mixsong/iy2ww24.html?frombaidu");
 
         ImageView imageView;
         imageView = findViewById(R.id.image);
@@ -37,18 +41,30 @@ public class NotifyActivity extends AppCompatActivity {
         Calendar calendar = Calendar.getInstance();
         int minute = calendar.get(Calendar.MINUTE);
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        if (0 < hour && 12 > hour) {
+            msg = "早上好 ^~^  现在是 " + hour + "时" + minute + "分";
+        } else if (18 < hour) {
+            msg = "晚上好 ^~^  现在是 " + hour + "时" + minute + "分";
+        } else {
+            msg = "下午好 ^~^  现在是 " + hour + "时" + minute + "分";
+        }
 
         SharedPreferences preferences = getSharedPreferences("com.example.notify.ALARM", Context.MODE_PRIVATE);
-
         if (preferences.getInt("hour", -1) == hour && preferences.getInt("minute", -1) == minute) {
             vibrate();
+            player = MediaPlayer.create(this, R.raw.music);
+            player.start();
             new AlertDialog.Builder(NotifyActivity.this)
                     .setTitle("闹钟")
-                    .setMessage("早上好 ^~^  现在是 " + hour + ":" + minute)
+                    .setMessage(msg)
                     .setPositiveButton("关闭闹铃", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             vibrator.cancel();
+                            if (player.isPlaying()) {
+                                player.stop();
+                                player.release();
+                            }
                         }
                     }).show();
         }
